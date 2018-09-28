cbb5cc9eb67e3bdf23090ae5c3551112775fd310
AvenChang <530685625@qq.com>
2018-09-28 10:33:59 +0800

    fancybox

--- src/main/java/club/williamleon/Test.java
+++ src/main/java/club/williamleon/Test.java
@@ -1,18 +1,12 @@
 package club.williamleon;
 
-import club.williamleon.config.Constant;
 import club.williamleon.util.MD5;
-import club.williamleon.util.val.GroupRole;
-import org.springframework.util.StringUtils;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.*;
 
 /**
  * Created by 53068 on 2018/4/24 0024.
@@ -28,10 +22,15 @@ public class Test {
 //        System.out.println(GroupRole.PASSER.ordinal());
 //        List<String> test = Arrays.asList("1", "2", "3");
 //        System.out.println(StringUtils.collectionToDelimitedString(test, ",", "'", "'"));
-        Test.todayMD5();
+
+        long timestamp = LocalDateTime.parse("2018-08-13T11:11:11")
+            .toEpochSecond(ZoneOffset.UTC);
+        Date d = new Date(1534158671);
+        System.out.println(System.currentTimeMillis());
     }
 
     private static DateFormat format = new SimpleDateFormat("yyyyMMdd");
+
     public static void todayMD5() {
         String user = "Mweprfre";
         String password = "98gheMjuo//*(ma";
